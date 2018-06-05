10d9c40cb28ecffb66558c698ac07ac2b24409ad
kevin wilkinson <willkillson@gmail.com>
2018-06-04 21:24:29 -0700

    Code in, need hashmap for output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,59 @@
 package com.company;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("hello world");
+        String input = "EbAAdbBEaBaaBBdAccbeebaec";
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        int playerA = 0;
+        int playerB = 0;
+        int playerC = 0;
+        int playerD = 0;
+        int playerE = 0;
+
+        int num = 0;
+        while(num!=input.length()){
+            switch(input.charAt(num)){
+                case 'A':
+                    playerA++;
+                    break;
+                case 'a':
+                    playerA--;
+                    break;
+                case 'B':
+                    playerB++;
+                    break;
+                case 'b':
+                    playerB--;
+                    break;
+                case 'C':
+                    playerC++;
+                    break;
+                case 'c':
+                    playerC--;
+                    break;
+                case 'D':
+                    playerD++;
+                    break;
+                case 'd':
+                    playerD--;
+                    break;
+                case 'E':
+                    playerE++;
+                    break;
+                case 'e':
+                    playerE--;
+                    break;
+            }
+
+            num++;
+        }
+
+
+        System.out.println("HelloWorld!!!!");
     }
 }
